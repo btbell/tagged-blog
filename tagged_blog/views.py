5323c953a74834382bf3255822e469a66f09e131
btbell <btbell@gmail.com>
2018-10-14 16:04:28 -0400

    added a time test

--- tagged_blog/views.py
+++ tagged_blog/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django import template
+
+import datetime
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("hello django!")
+
+def time(request):
+    now = datetime.datetime.now()
+    t = template.Template("<html><body>Current date and time {{ now }}</body></html>")
+    c = template.Context({'now': now})
+    html = t.render(c)
+    return HttpResponse(html)
